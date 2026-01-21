d39f303364991d94ab768e10f20e1ef14d9c712b
Kevin65536 <qdsxhkw@126.com>
2026-01-21 17:53:36 +0800

    Implementing neurorvq tokenizer

--- src/tokenizers/__init__.py
+++ src/tokenizers/__init__.py
@@ -8,6 +8,13 @@ from .fsq import FSQTokenizer
 from .vqvae import VQVAETokenizer
 from .patch_vqvae import PatchVQVAETokenizer
 from .freq_patch_vqvae import FreqDomainPatchVQVAE, FreqDomainPatchVQVAE_V2
+from .neurorvq import (
+    NeuroRVQTokenizer,
+    NeuroRVQTokenizer_V2,
+    NormEMAVectorQuantizer,
+    ResidualVectorQuantization,
+    MultiScaleTemporalEncoder,
+)
 
 __all__ = [
     'BaseTokenizer', 
@@ -16,4 +23,9 @@ __all__ = [
     'PatchVQVAETokenizer',
     'FreqDomainPatchVQVAE',
     'FreqDomainPatchVQVAE_V2',
+    'NeuroRVQTokenizer',
+    'NeuroRVQTokenizer_V2',
+    'NormEMAVectorQuantizer',
+    'ResidualVectorQuantization',
+    'MultiScaleTemporalEncoder',
 ]
